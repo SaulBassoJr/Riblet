671742a91dfc945b2c6b539e26808cdee7503cec
Saul Basso Junior <85260541+SaulBassoJr@users.noreply.github.com>
2022-07-11 16:03:59 -0300

    Add files via upload

---
+++ Riblet/src/MyFinishWindow.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Saul
+ */
+import java.awt.event.*;
+
+   public class MyFinishWindow extends WindowAdapter
+   {
+     public void windowClosing(WindowEvent e)
+     {
+       System.exit(0);
+     }
+   }
